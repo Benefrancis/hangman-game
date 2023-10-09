6637175722ad8ea8fd1ab30be9be31bb057447ad
Benefrancis do Nascimento <benefrancis@gmail.com>
2023-10-09 19:15:45 -0300

    dividindo os arquivos

---
+++ cmake-build-debug/CMakeFiles/forca.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/forca.dir/acertou.cpp.obj"
+  "CMakeFiles/forca.dir/acertou.cpp.obj.d"
+  "CMakeFiles/forca.dir/adiciona_palavra.cpp.obj"
+  "CMakeFiles/forca.dir/adiciona_palavra.cpp.obj.d"
+  "CMakeFiles/forca.dir/cabecalho.cpp.obj"
+  "CMakeFiles/forca.dir/cabecalho.cpp.obj.d"
+  "CMakeFiles/forca.dir/chute.cpp.obj"
+  "CMakeFiles/forca.dir/chute.cpp.obj.d"
+  "CMakeFiles/forca.dir/chutes_errados.cpp.obj"
+  "CMakeFiles/forca.dir/chutes_errados.cpp.obj.d"
+  "CMakeFiles/forca.dir/enforcou.cpp.obj"
+  "CMakeFiles/forca.dir/enforcou.cpp.obj.d"
+  "CMakeFiles/forca.dir/existe.cpp.obj"
+  "CMakeFiles/forca.dir/existe.cpp.obj.d"
+  "CMakeFiles/forca.dir/forca.cpp.obj"
+  "CMakeFiles/forca.dir/forca.cpp.obj.d"
+  "CMakeFiles/forca.dir/imprime_palavra.cpp.obj"
+  "CMakeFiles/forca.dir/imprime_palavra.cpp.obj.d"
+  "CMakeFiles/forca.dir/ler_arquivo.cpp.obj"
+  "CMakeFiles/forca.dir/ler_arquivo.cpp.obj.d"
+  "CMakeFiles/forca.dir/placar.cpp.obj"
+  "CMakeFiles/forca.dir/placar.cpp.obj.d"
+  "CMakeFiles/forca.dir/salva_arquivo.cpp.obj"
+  "CMakeFiles/forca.dir/salva_arquivo.cpp.obj.d"
+  "CMakeFiles/forca.dir/sorteia.cpp.obj"
+  "CMakeFiles/forca.dir/sorteia.cpp.obj.d"
+  "forca.exe"
+  "forca.exe.manifest"
+  "forca.pdb"
+  "libforca.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/forca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
